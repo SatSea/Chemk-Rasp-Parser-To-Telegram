0eb1080c032bc9cf1e4f2e4ccf1731045a362b32
SatSea <staspetrov388@gmail.com>
2023-02-18 22:59:10 +0300

    Small fix

--- dispatcher/rasp_checker.py
+++ dispatcher/rasp_checker.py
@@ -19,13 +19,13 @@ async def start_waiting():
         weekday_number: int = datetime.today().weekday()
         time_now: datetime = datetime.now()
         time_start_hour: datetime = time_now.replace(**accuracy_to_hours)
-        match(weekday_number):
-            case 5:
-                time_to_sleep = time_start_hour + timedelta(2) - time_now
-            case _:
-                if datetime.today().hour < hour_when_start_checking:
-                    time_to_sleep = time_start_hour - time_now
-                else:
+        if datetime.today().hour < hour_when_start_checking and weekday_number != 6:
+            time_to_sleep = time_start_hour - time_now
+        else:
+            match(weekday_number):
+                case 5:
+                    time_to_sleep = time_start_hour + timedelta(2) - time_now
+                case _:
                     time_to_sleep = time_start_hour + timedelta(1) - time_now
         seconds_to_sleep: int = time_to_sleep.total_seconds()
         logger.info(f"Need to sleep for {round(seconds_to_sleep)} seconds")
